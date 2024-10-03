91c5ad4c0af0a1100fc1ad9d9f3d2b7ca045e1e4
nhnn3106 <120386489+nhnn3106@users.noreply.github.com>
2024-10-04 00:17:53 +0700

    create java project

---
+++ src/main/java/com/mycompany/hotelmanagementproject/HotelManagementProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelmanagementproject;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HotelManagementProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
