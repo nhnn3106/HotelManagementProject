8306b7b05648bbedbf1b8a2d6dd1dc0922e9517e
nhnn3106 <120386489+nhnn3106@users.noreply.github.com>
2024-10-06 14:00:02 +0700

    phat

---
+++ src/GUI/QuanLyKhachSan.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quanlykhachsan;
+
+/**
+ *
+ * @author Admin
+ */
+public class QuanLyKhachSan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ĐP design = new ĐP();
+        design.setLocationRelativeTo(null);
+        design.setVisible(true);
+           
+    }
+    
+}
